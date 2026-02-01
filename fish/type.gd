5cdddb0acf744f76f9880d991c682beb7851d7ac
hanss-prog <hanpeer211@gmail.com>
2026-02-01 14:53:59 +0800

    Add files via upload

---
+++ fish/type.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var sentences = [
+	"the fish is fighting, u got this:3",
+	"keep the line steady",
+	"do not panic and type carefully",
+    "the rod is bending rahhhhhhh"
+]
+
+var current_sentence := ""
+var typed_text := ""
